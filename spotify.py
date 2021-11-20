07e21aea936bd7a32dd522acb7f354abcfddd7db
Abel Asfaw <abel.f.asfaw@gmail.com>
2021-11-20 16:30:39 -0500

    first commmit

---
+++ spotify.py
@@ -0,0 +1,104 @@
+import requests
+import datetime
+from urllib.parse import urlencode
+import base64
+
+client_id = "3fa60b2c4d12433196915ed0092b5b93"
+client_secret = "284c97190e7e45a1a8995ba370baf2ab"
+
+class SpotifyAPI(object):
+    access_token = None
+    access_token_expires = datetime.datetime.now()
+    access_token_did_expire = True
+    client_id = None
+    client_secret = None
+    token_url = "https://accounts.spotify.com/api/token"
+    
+    def __init__(self, client_id, client_secret, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.client_id = client_id
+        self.client_secret = client_secret
+
+    def get_client_credentials(self):
+        """
+        Returns a base64 encoded string
+        """
+        client_id = self.client_id
+        client_secret = self.client_secret
+        if client_secret == None or client_id == None:
+            raise Exception("You must set client_id and client_secret")
+        client_creds = f"{client_id}:{client_secret}"
+        client_creds_b64 = base64.b64encode(client_creds.encode())
+        return client_creds_b64.decode()
+    
+    def get_token_headers(self):
+        client_creds_b64 = self.get_client_credentials()
+        return {
+            "Authorization": f"Basic {client_creds_b64}"
+        }
+    
+    def get_token_data(self):
+        return {
+            "grant_type": "client_credentials"
+        } 
+    
+    def perform_auth(self):
+        token_url = self.token_url
+        token_data = self.get_token_data()
+        token_headers = self.get_token_headers()
+        r = requests.post(token_url, data=token_data, headers=token_headers)
+        if r.status_code not in range(200, 299):
+            return False
+        data = r.json()
+        now = datetime.datetime.now()
+        access_token = data['access_token']
+        expires_in = data['expires_in'] # seconds
+        expires = now + datetime.timedelta(seconds=expires_in)
+        self.access_token = access_token
+        self.access_token_expires = expires
+        self.access_token_did_expire = expires < now
+        return True
+
+spotify = SpotifyAPI(client_id, client_secret)
+spotify.perform_auth()
+       
+access_token = spotify.access_token
+
+headers = {
+    "Authorization": f"Bearer {access_token}"
+}
+
+GET_ARTIST_ENDPOINT = 'https://api.spotify.com/v1/artists/{id}'
+SEARCH_ENDPOINT = 'https://api.spotify.com/v1/search'
+RELATED_ARTISTS_ENDPOINT = 'https://api.spotify.com/v1/artists/{id}/related-artists'
+TOP_TRACKS_ENDPOINT = 'https://api.spotify.com/v1/artists/{id}/top-tracks'
+
+# https://developer.spotify.com/web-api/get-artist/
+def get_artist(artist_id):
+    url = GET_ARTIST_ENDPOINT.format(id=artist_id)
+    resp = requests.get(url,headers = headers)
+    return resp.json()
+
+
+# https://developer.spotify.com/web-api/search-item/
+def search_by_artist_name(name):
+    myparams = {'type': 'artist'}
+    myparams['q'] = name
+    resp = requests.get(SEARCH_ENDPOINT, params=myparams,headers = headers)
+    return resp.json()
+
+
+# https://developer.spotify.com/web-api/get-related-artists/
+def get_related_artists(artist_id):
+    url = RELATED_ARTISTS_ENDPOINT.format(id=artist_id)
+    resp = requests.get(url,headers = headers)
+    return resp.json()
+
+# https://developer.spotify.com/web-api/get-artists-top-tracks/
+def get_artist_top_tracks(artist_id, country='US'):
+    url = TOP_TRACKS_ENDPOINT.format(id=artist_id)
+    myparams = {'country': country}
+    resp = requests.get(url, params=myparams,headers = headers)
+    return resp.json()
+
+
