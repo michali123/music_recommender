a5fabefbc5cd0de76cf16c36311615ddcbe290e0
AbelNyc <53309012+AbelNyc@users.noreply.github.com>
2021-12-12 22:10:13 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -19,6 +19,9 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 import pandas as pd
 from musicnn.tagger import top_tags
+from tempocnn.classifier import TempoClassifier
+from tempocnn.feature import read_features
+
 import os
 
 # from secrets import PULSOID_SECRET_KEY
@@ -28,10 +31,10 @@ app = Flask(__name__)
 app.secret_key = "rfgt535"
 # app.config[SESSION_COOKIE_NAME]='Michal'
 
-
 @app.route("/")  # goes to main page
 def index():
     tokenData = getPulsoidToken()
+    print(tokenData['scopes'][0])
     getPulsoidHR()  
     heartRate = getPulsoidHR()
     access_token = getTokenInfo()
@@ -42,16 +45,11 @@ def index():
         print(json.dumps(user, sort_keys=True, indent=4))
     return render_template("record.html", hr=heartRate, checkPulsoid=getPulsoidConnecetionStatus())
 
-
-
-
-
 @app.route('/login')
 def login():
     sp_oauth = create_spotify_oauth()
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
-
 
 
 
@@ -73,10 +71,6 @@ def getTokenInfo():
         token_info = sp_oauth.refresh_access_token(
             session.get('token_info').get('refresh_token'))
     return access_token
-
-
-
-
 
 @app.route('/playlistRecommender', methods=['GET', 'POST'])
 def playlistRecommender():
@@ -108,6 +102,122 @@ def playlistRecommender():
         # return jsonify(playlist) #jsonify lets us return a list
         return render_template("spotifyRecommender.html", playlist_embed_link=playlist_embed_link)
 # jsonfile=json.dumps(jsonTracks)
+
+def create_spotify_oauth():
+    return SpotifyOAuth(
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        redirect_uri=url_for('playlistRecommender', _external=True),
+        scope="user-top-read user-read-recently-played user-library-read user-read-currently-playing playlist-read-collaborative playlist-read-private playlist-modify-public user-modify-playback-state user-read-playback-state")
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
+
+# Shut down the scheduler when exiting the app
+# atexit.register(lambda: scheduler.shutdown())
+
+
+    
+@app.route('/artist/<id>', methods=['GET', 'POST'])
+def artist(id):
+
+
+    artist = spotify.get_artist(id)
+    if artist['images']:
+        image_url = artist['images'][0]['url']
+    else:
+        image_url = 'http://placecage.com/600/400'
+
+    tracksdata = spotify.get_artist_top_tracks(id)
+    tracks = tracksdata['tracks']
+
+    artistsdata = spotify.get_related_artists(id)
+
+    audio_features=[]
+
+    for i in range(10):
+      for k,v in tracks[i].items():
+          if k == "id":
+                audio_features.append(search_audio_features(v))
+
+    predictions_list = []
+    predicted_tags = []
+    regr(audio_features, predictions_list)
+
+    #final_df = compare_features(audio_features)
+    
+
+    tokenData = getPulsoidToken()
+    heartRate = getPulsoidHR()
+    
+    relartists = artistsdata['artists']
+
+    # predict heart - rate 
+    model_name = 'cnn'
+    if len(os.listdir(r"C:\Users\abelf\Downloads\music_recommender\audio downloaded")) > 0:
+
+        music = os.listdir(r"C:\Users\abelf\Downloads\music_recommender\audio downloaded")[-1]
+        path_folder = "C:\\Users\\abelf\\Downloads\\music_recommender\\audio downloaded"+ "\\" + music
+
+        # initialize the model (may be re-used for multiple files)
+        classifier = TempoClassifier(model_name)
+
+        # read the file's features
+        features = read_features(path_folder)
+
+        # estimate the global tempo
+        tempo = classifier.estimate_tempo(features, interpolate=False)
+
+
+
+    # predict tags 
+    if len(os.listdir(r"C:\Users\abelf\Downloads\music_recommender\audio downloaded")) > 0:
+        music = os.listdir(r"C:\Users\abelf\Downloads\music_recommender\audio downloaded")[-1]
+        path_folder = "C:\\Users\\abelf\\Downloads\\music_recommender\\audio downloaded"+ "\\" + music
+
+        os.rename(path_folder,path_folder.strip(".wav") + ".mp3")
+        music = os.listdir(r"C:\Users\abelf\Downloads\music_recommender\audio downloaded")[-1]
+        path_folder = "C:\\Users\\abelf\\Downloads\\music_recommender\\audio downloaded"+ "\\" + music
+
+        predicted_tags= top_tags(path_folder, model='MTT_musicnn', topN=3)
+        os.remove(path_folder)
+
+   
+    if len(predicted_tags)>0: 
+        html = render_template('record.html',
+                                hr = heartRate,
+                                checkSpotify=get_spotify_connnection_stauts(),
+                                checkPulsoid=getPulsoidConnecetionStatus(),
+                                artist=artist,
+                                related_artists=relartists,
+                                image_url=image_url,
+                                tracks=tracks,
+                                predictions_list = predictions_list, 
+                                predicted_tags = predicted_tags,
+                                tempo = tempo)
+    else:
+        html = render_template('record.html',
+                                hr = heartRate,
+                                checkSpotify=get_spotify_connnection_stauts(),
+                                checkPulsoid=getPulsoidConnecetionStatus(),
+                                artist=artist,
+                                related_artists=relartists,
+                                image_url=image_url,
+                                tracks=tracks,
+                                predictions_list = predictions_list, 
+                                )
+
+    return html
+
+
+
+
+
+
+
+
 
 # custom playlist based on artist search
 @app.route('/playlistRecommender-artist', methods=['GET', 'POST'])
@@ -146,16 +256,6 @@ def playlistRecommenderforartist():
 
 
 
-def create_spotify_oauth():
-    return SpotifyOAuth(
-        client_id=CLIENT_ID,
-        client_secret=CLIENT_SECRET,
-        redirect_uri=url_for('playlistRecommender', _external=True),
-        scope="user-top-read user-read-recently-played user-library-read user-read-currently-playing playlist-read-collaborative playlist-read-private playlist-modify-public user-modify-playback-state user-read-playback-state")
-
-
-
-
 
 
 
@@ -163,77 +263,6 @@ def create_spotify_oauth():
 
 # Shut down the scheduler when exiting the app
 # atexit.register(lambda: scheduler.shutdown())
-
-
-@app.route('/artist/<id>', methods=['GET', 'POST'])
-def artist(id):
-
-
-    artist = spotify.get_artist(id)
-    if artist['images']:
-        image_url = artist['images'][0]['url']
-    else:
-        image_url = 'http://placecage.com/600/400'
-
-    tracksdata = spotify.get_artist_top_tracks(id)
-    tracks = tracksdata['tracks']
-
-    artistsdata = spotify.get_related_artists(id)
-
-    audio_features=[]
-
-    for i in range(10):
-      for k,v in tracks[i].items():
-          if k == "id":
-                audio_features.append(search_audio_features(v))
-
-    predictions_list = []
-    predicted_tags = []
-    regr(audio_features, predictions_list)
-
-    #final_df = compare_features(audio_features)
-    
-
-    tokenData = getPulsoidToken()
-    heartRate = getPulsoidHR()
-    
-    relartists = artistsdata['artists']
-    if len(os.listdir(r"C:\Users\abelf\Downloads\music_recommender\audio downloaded")) > 0:
-        music = os.listdir(r"C:\Users\abelf\Downloads\music_recommender\audio downloaded")[-1]
-        path_folder = "C:\\Users\\abelf\\Downloads\\music_recommender\\audio downloaded"+ "\\" + music
-
-        os.rename(path_folder,path_folder.strip(".wav") + ".mp3")
-        music = os.listdir(r"C:\Users\abelf\Downloads\music_recommender\audio downloaded")[-1]
-        path_folder = "C:\\Users\\abelf\\Downloads\\music_recommender\\audio downloaded"+ "\\" + music
-
-        predicted_tags= top_tags(path_folder, model='MTT_musicnn', topN=3)
-        os.remove(path_folder)
-
-   
-    if len(predicted_tags)>0: 
-        html = render_template('record.html',
-                                hr = heartRate,
-                                checkSpotify=get_spotify_connnection_stauts(),
-                                checkPulsoid=getPulsoidConnecetionStatus(),
-                                artist=artist,
-                                related_artists=relartists,
-                                image_url=image_url,
-                                tracks=tracks,
-                                predictions_list = predictions_list, 
-                                predicted_tags = predicted_tags)
-    else:
-        html = render_template('record.html',
-                                hr = heartRate,
-                                checkSpotify=get_spotify_connnection_stauts(),
-                                checkPulsoid=getPulsoidConnecetionStatus(),
-                                artist=artist,
-                                related_artists=relartists,
-                                image_url=image_url,
-                                tracks=tracks,
-                                predictions_list = predictions_list, 
-                                )
-
-    return html
 
 
 
--- static/app.js
+++ static/app.js
@@ -25,3 +25,16 @@ const mockHeartRate = () => {
 
     return heartRate
 }
+
+$(window).scroll(function() {
+    var winScrollTop = $(window).scrollTop();
+    var winHeight = $(window).height();
+    var floaterHeight = $('#floater').outerHeight(true);
+    //true so the function takes margins into account
+    var fromBottom = 20;
+
+    var top = winScrollTop + winHeight - floaterHeight - fromBottom;
+    $('#floater').css({'top': top + 'px'});
+});
+
+const redirectUri = 'http://localhost:8888/callback';
